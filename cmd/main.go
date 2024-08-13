a59e4bbe73dd64f8d325145c5d1475712c2727b0
ppp3ppj <ppp.3ppj@gmail.com>
2024-08-13 22:06:43 +0700

    add run server echo

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"github.com/ppp3ppj/htmx-hardware-monitor-ws/config"
+	"github.com/ppp3ppj/htmx-hardware-monitor-ws/internal/server"
+)
 
+func main() {
+    conf := config.ConfigGetting()
+    s := server.NewEchoServer(conf)
+    s.Start()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,11 +1,19 @@
 package server
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
 	"github.com/ppp3ppj/htmx-hardware-monitor-ws/config"
+	"github.com/ppp3ppj/htmx-hardware-monitor-ws/internal/middlewares"
 )
 
 type echoServer struct {
@@ -32,4 +40,48 @@ func NewEchoServer(conf *config.Config) *echoServer {
     return server
 }
 
+func (s * echoServer) Start() {
+    timeOutMiddleware := middlewares.TimeOutMiddleware(s.conf.Server.Timeout)
+    corsMiddleware := middlewares.CORSMiddleware(s.conf.Server.AllowOrigins)
 
+    s.app.Use(middleware.Recover())
+    s.app.Use(middleware.Logger())
+
+    s.app.Use(timeOutMiddleware)
+    s.app.Use(corsMiddleware)
+
+    s.app.Static("/dist", "dist")
+    s.app.Static("/assets", "public/assets")
+
+    // for health check
+    s.app.GET("/v1/health", s.healthCheck)
+
+    // Graceful Shutdown
+    quitCh := make(chan os.Signal, 1)
+    signal.Notify(quitCh, syscall.SIGINT, syscall.SIGTERM)
+    go s.gracefullyShutdown(quitCh)
+
+    s.httpListening()
+}
+
+func (s *echoServer) gracefullyShutdown(quitCh <-chan os.Signal) {
+    ctx := context.Background()
+
+    <-quitCh
+    s.app.Logger.Info("Shutting down the service...")
+
+    if err := s.app.Shutdown(ctx); err != nil {
+        s.app.Logger.Fatalf("Error: %s", err.Error())
+    }
+}
+
+func (s *echoServer) httpListening() {
+    url := fmt.Sprintf(":%d", s.conf.Server.Port)
+    if err := s.app.Start(url); err != nil && err != http.ErrServerClosed {
+        s.app.Logger.Fatalf("shutting down the server: %v", err)
+    }
+}
+
+func (s *echoServer) healthCheck(c echo.Context) error {
+    return c.String(http.StatusOK, "OK")
+}
