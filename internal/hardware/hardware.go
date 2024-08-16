3cc9acba8cbfad231ea3a2e5b2569ca67a0fb92a
ppp3ppj <ppp.3ppj@gmail.com>
2024-08-16 22:38:40 +0700

    add hardware info example

---
+++ internal/hardware/hardware.go
@@ -0,0 +1,46 @@
+package hardware
+
+import (
+	"runtime"
+	"strconv"
+
+	"github.com/shirou/gopsutil/v4/host"
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
+type SystemInfo struct {
+    Os string
+    Platform string
+    Hostname string
+    NumberOfProcess string
+    TotalMem string
+    FreeMem string
+    PercentageUsedMem string
+}
+
+const megabyteDiv uint64 = 1024 * 1024
+const gigabyteDiv uint64 = megabyteDiv * 1024
+
+func GetSystemSection() (SystemInfo, error) {
+    runTimeOs := runtime.GOOS
+
+    vmStat, err := mem.VirtualMemory()
+    if err != nil {
+        return SystemInfo{}, err
+    }
+
+    hostStat, err := host.Info()
+    if err != nil {
+        return SystemInfo{}, err
+    }
+
+    return SystemInfo{
+        Os: runTimeOs,
+        Platform: hostStat.Platform,
+        Hostname: hostStat.Hostname,
+        NumberOfProcess: strconv.FormatUint(hostStat.Procs, 10),
+        TotalMem: strconv.FormatUint(vmStat.Total/megabyteDiv, 10),
+        FreeMem: strconv.FormatUint(vmStat.Free/megabyteDiv, 10),
+        PercentageUsedMem: strconv.FormatFloat(vmStat.UsedPercent, 'f', 2, 64),
+    }, nil
+}
--- pkg/dashboard/dashboard_frontend.go
+++ pkg/dashboard/dashboard_frontend.go
@@ -1,9 +1,11 @@
 package dashboard
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/ppp3ppj/htmx-hardware-monitor-ws/internal/hardware"
 	"github.com/ppp3ppj/htmx-hardware-monitor-ws/template"
 	views_pages "github.com/ppp3ppj/htmx-hardware-monitor-ws/views/pages/index"
 	views_variables "github.com/ppp3ppj/htmx-hardware-monitor-ws/views/variables"
@@ -24,6 +26,9 @@ func (fe *DashboardFrontend) Index(c echo.Context) error {
         ExtraHeaders: nil,
         Component: nil,
     }
+    fmt.Println("******* Test *********")
+    fmt.Println(hardware.GetSystemSection())
+    fmt.Println("******* End *********")
 
     index := views_pages.Index(bodyOpts)
     return template.AssertRender(c, http.StatusOK, index)
