346bba411ed77b762c4a16d549fe6b29250c5fb1
ppp3ppj <ppp.3ppj@gmail.com>
2024-08-17 21:11:06 +0700

    add hero landing page

--- views/pages/index/index_templ.go
+++ views/pages/index/index_templ.go
@@ -57,7 +57,7 @@ func index() templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div>Hi</div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<div class=\"hero min-h-screen\" style=\"background-image: url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp);\"><div class=\"hero-overlay bg-opacity-60\"></div><div class=\"hero-content text-neutral-content text-center\"><div class=\"max-w-md\"><h1 class=\"mb-5 text-5xl font-bold\">Hello there</h1><p class=\"mb-5\">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p><button class=\"btn btn-primary\">Get Started</button></div></div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
